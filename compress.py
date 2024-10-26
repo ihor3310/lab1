a1ac2c438b6d26e168611c6a0e7e18f77a7b6577
ihor3310 <96204825+ihor3310@users.noreply.github.com>
2024-10-26 12:41:31 +0300

    adding main inputs
    ihor

--- compress.py
+++ compress.py
@@ -2,3 +2,11 @@ import datetime
 import zipfile
 import gzip
 import pathlib
+
+
+
+if __name__ == "__main__":
+    source_f = input("Source file: ")
+    output_d = input("Output directory: ")
+    archive_t = input("Archive type (gz/zip): ")
+    compress_file(source_f, output_d, archive_t)
