5618e0ed4b9e5aac5ef9ccd5a00aa3b04652ec31
ihor3310 <96204825+ihor3310@users.noreply.github.com>
2024-10-26 16:55:24 +0300

    adding compress function
    ihor

--- compress.py
+++ compress.py
@@ -3,10 +3,12 @@ import zipfile
 import gzip
 import pathlib
 
-
+def cmprs_f(filename, out_dir, archive_type="gz"):
+    archive_name = generate_archive_name(filename)
+    output_path = pathlib.Path(out_dir) / f"{archive_name}.{archive_type}"
 
 if __name__ == "__main__":
     source_f = input("Source file: ")
     output_d = input("Output directory: ")
     archive_t = input("Archive type (gz/zip): ")
-    compress_file(source_f, output_d, archive_t)
+    cmprs_f(source_f, output_d, archive_t)
