97a38ca0c47d93476c37b3df4fbd858f571cf9a5
ihor3310 <96204825+ihor3310@users.noreply.github.com>
2024-10-27 17:47:47 +0200

    working with the file system

--- compress.py
+++ compress.py
@@ -13,6 +13,17 @@ def gen_arch_name(filename):
 def cmprs_f(filename, out_dir, archive_type="gz"):
     archive_name = gen_arch_name(filename)
     output_path = pathlib.Path(out_dir) / f"{archive_name}.{archive_type}"
+    if archive_type == "gz":
+        with open(filename, "rb") as f_in, gzip.open(output_path, "wb") as f_out:
+            while True:
+                data = f_in.read(1024)
+                if not data:
+                    break
+                f_out.write(data)
+    elif archive_type == "zip":
+        with zipfile.ZipFile(output_path, "w") as archive:
+            archive.write(filename, arcname=pathlib.Path(filename).name)
+    print(f"File compressed as: {output_path}")
 
 if __name__ == "__main__":
     source_f = input("Source file: ")
