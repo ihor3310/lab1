4252a1bcb9966a6de393a9f0a173bcfefae1ca83
rroleg <o.pochuev@ukma.edu.ua>
2024-10-27 23:19:33 +0300

    rrUpdate decompress.py

--- decompress.py
+++ decompress.py
@@ -8,6 +8,10 @@ def decompress_file(archive_path, output_dir):
         with gzip.open(archive_path, "rb") as f_in, open(output_path, "wb") as f_out:
             while True:
                 data = f_in.read(1024)
+                if not data:
+                    break
+                f_out.write(data)
+    
 if __name__ == "__main__":
     archive_path = input("Source archive file: ")
     output_dir = input("Output directory: ")
