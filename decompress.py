687d367df8712f3692d086a52cbbfa07c09b41da
rroleg <o.pochuev@ukma.edu.ua>
2024-10-27 23:16:34 +0300

    rrUpdate decompress.py

--- decompress.py
+++ decompress.py
@@ -3,9 +3,10 @@ import zipfile
 import pathlib
 def decompress_file(archive_path, output_dir):
     archive_type = pathlib.Path(archive_path).suffix
+    if archive_type == ".gz":
+        output_path = pathlib.Path(output_dir) / pathlib.Path(archive_path).stem
 if __name__ == "__main__":
     archive_path = input("Source archive file: ")
     output_dir = input("Output directory: ")
     decompress_file(archive_path, output_dir)
-if archive_type == ".gz":
-        output_path = pathlib.Path(output_dir) / pathlib.Path(archive_path).stem
+
