ffcce719c71904a22f162ce43fb3c1ec218c59b2
ihor3310 <96204825+ihor3310@users.noreply.github.com>
2024-10-27 18:25:38 +0200

    adding main code

--- backup.py
+++ backup.py
@@ -3,3 +3,8 @@ import tarfile
 import datetime
 import pathlib
 import argparse
+
+if __name__ == "__main__":
+    prsr = argparse.ArgumentParser(description="Backup or restore files.")
+    prsr.add_argument("operation", choices=["c", "d"], help="Operation: 'c' to compress or 'd' to decompress.")
+    args = prsr.parse_args()
