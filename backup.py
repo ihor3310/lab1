68d5afbdf06df93c31198e6caf39ac2fe42796dc
rroleg <o.pochuev@ukma.edu.ua>
2024-10-27 23:13:35 +0300

    rrUpdate backup.py

--- backup.py
+++ backup.py
@@ -26,4 +26,5 @@ if __name__ == "__main__":
     prsr = argparse.ArgumentParser(description="Backup or restore files.")
     prsr.add_argument("operation", choices=["c", "d"], help="Operation: 'c' to compress or 'd' to decompress.")
     args = prsr.parse_args()
-    
+    if args.operation == "c":
+        create_backup(
