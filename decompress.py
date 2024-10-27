3b7ceff6d7894fbd73436f10b3846ba3bddb6232
rroleg <o.pochuev@ukma.edu.ua>
2024-10-27 23:17:04 +0300

    rrUpdate decompress.py

--- decompress.py
+++ decompress.py
@@ -5,6 +5,9 @@ def decompress_file(archive_path, output_dir):
     archive_type = pathlib.Path(archive_path).suffix
     if archive_type == ".gz":
         output_path = pathlib.Path(output_dir) / pathlib.Path(archive_path).stem
+        with gzip.open(archive_path, "rb") as f_in, open(output_path, "wb") as f_out:
+            while True:
+                data = f_in.read(1024)
 if __name__ == "__main__":
     archive_path = input("Source archive file: ")
     output_dir = input("Output directory: ")
