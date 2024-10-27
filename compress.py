f663bc43d2019551ed6ae0b7d954b577995322c3
ihor3310 <96204825+ihor3310@users.noreply.github.com>
2024-10-27 17:34:16 +0200

    adding new function

--- compress.py
+++ compress.py
@@ -3,8 +3,15 @@ import zipfile
 import gzip
 import pathlib
 
+def gen_arch_name(filename):
+    date = datetime.datetime.now().strftime("%Y%m%d")
+    archive_number = 1
+    base_n = pathlib.Path(filename).stem
+    archive_name = f"{base_n}_{date}_{archive_number}"
+    return archive_name
+
 def cmprs_f(filename, out_dir, archive_type="gz"):
-    archive_name = generate_archive_name(filename)
+    archive_name = gen_arch_name(filename)
     output_path = pathlib.Path(out_dir) / f"{archive_name}.{archive_type}"
 
 if __name__ == "__main__":
