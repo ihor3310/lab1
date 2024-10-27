f3782a3b564619638a4541f3d6c84d6bc5ac4a04
rroleg <o.pochuev@ukma.edu.ua>
2024-10-27 23:15:45 +0300

    rrUpdate decompress.py

--- decompress.py
+++ decompress.py
@@ -3,3 +3,7 @@ import zipfile
 import pathlib
 def decompress_file(archive_path, output_dir):
     archive_type = pathlib.Path(archive_path).suffix
+if __name__ == "__main__":
+    archive_path = input("Source archive file: ")
+    output_dir = input("Output directory: ")
+    decompress_file(archive_path, output_dir)
